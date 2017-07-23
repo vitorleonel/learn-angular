f0e42fdb50306876832f39250028517ecf059395
Vitor Leonel <vitorleoneloficial@gmail.com>
2017-07-23 12:19:41 -0300

    escrevendo e lendo dados no firebase

--- src/app/event/create-event/create-event.component.ts
+++ src/app/event/create-event/create-event.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, NgForm, Validators } from '@angular/forms';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
@@ -10,20 +12,22 @@ export class CreateEventComponent implements OnInit {
 
   form: any;
 
-  events: Array<Object> = [];
+  events: any = [];
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private angularFire: AngularFireDatabase) { }
+
+  ngOnInit() {
     this.form = this.formBuilder.group({
       'name': ['', Validators.required],
       'address': ['', Validators.required],
       'description': ['', Validators.required]
     });
+
+    this.events = this.angularFire.list('events');
   }
 
-  ngOnInit() { }
-
   onSubmit() {
-    this.events.push({
+    this.angularFire.list('events').push({
       name: this.form.value.name,
       address: this.form.value.address,
       description: this.form.value.description,
